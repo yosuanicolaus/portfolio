7923aa1e778b2ac5d427e6f01631d8cab7473d66
Yosua Nicolaus <yosuanicolaus@gmail.com>
2023-02-23 19:27:33 +1100

    Create homepage welcome texts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
 export default function Home() {
   return (
-    <div>
-      <div>Hello, world!</div>
-      <button className="btn">Test Button</button>
+    <div className="min-h-screen p-4">
+      <nav className="flex border-b sticky">
+        <div className="w-8 h-8 bg-primary ml-auto rounded-full mb-4"></div>
+      </nav>
+      <section id="hi">
+        <div className="text-3xl my-4 font-bold">Hello.</div>
+      </section>
+      <section className="mr-8">
+        <div className="text-3xl mb-4">
+          I'm <b>Yosua Nicolaus</b>, a self-taught frontend developer based in
+          Sydney, Australia. I <b>love</b> to build <b>projects</b> and{" "}
+          <b>learn</b> something new.
+        </div>
+      </section>
+      <section>
+        <div className="text-3xl mb-4">
+          I enjoy creating <b>complex</b> software from <b>scratch</b>. Take a
+          look at some of my projects:
+        </div>
+      </section>
     </div>
   );
 }
