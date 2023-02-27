3f0d179adda392f60a07e6f08679d94b6385948e
Yosua Nicolaus <yosuanicolaus@gmail.com>
2023-02-27 20:15:54 +1100

    style Project card

--- src/app/(FeaturedProjects).tsx
+++ src/app/(FeaturedProjects).tsx
@@ -13,8 +13,16 @@ export function FeaturedProjects() {
 
 function Project({ name, description, image, github, live }: ProjectProp) {
   return (
-    <div className="w-full border shadow">
+    <a
+      target="_blank"
+      href={live ? live : github}
+      className="w-full border shadow relative"
+    >
+      <div className="absolute w-full h-64 lg:h-80 grid place-content-center hover:bg-green-700 hover:opacity-90 opacity-0 transition text-white">
+        <h1 className="opacity-100 font-bold text-lg text-center">{name}</h1>
+        <div className="mx-8">{description}</div>
+      </div>
       <img className="object-cover h-64 w-full lg:h-80" src={image}></img>
-    </div>
+    </a>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
       */}
       <head />
       <body>
-        <nav className="flex border-b sticky top-0 p-4 bg-white shadow-sm">
+        <nav className="flex border-b sticky top-0 p-4 bg-white shadow-sm z-20">
           <div className="w-8 h-8 bg-primary ml-auto rounded-full"></div>
         </nav>
         <>{children}</>
