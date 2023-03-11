c9687aeaa958b17f9f3478cf5ccbc14c11ebaddd
Yosua Nicolaus <yosuanicolaus@gmail.com>
2023-03-11 13:26:29 +1100

    replace Project card link to div
    To support mobile and prevent them from accidentally
    clicking link when trying to look at the description,
    I will replace the big green/blue link background link section
    with a div
    And then, have the real link for opening the link at the bottom.

--- src/app/(FeaturedProjects).tsx
+++ src/app/(FeaturedProjects).tsx
@@ -27,9 +27,7 @@ function Project({ name, description, image, github, live }: ProjectProp) {
           <h1 className="opacity-100 font-bold text-lg text-center">{name}</h1>
           <div className="mx-3 md:mx-8 text-sm md:text-base">{description}</div>
         </div>
-        <a
-          href={github}
-          target="_blank"
+        <div
           className="bg-green-700 transition h-64 w-[1000px] lg:h-80 flex justify-center items-center"
           style={{ opacity: active ? 0.9 : 0 }}
         >
@@ -47,14 +45,16 @@ function Project({ name, description, image, github, live }: ProjectProp) {
               <path d="M26.484 91.806c-.133.3-.605.39-1.035.185-.44-.196-.685-.605-.543-.906.13-.31.603-.395 1.04-.188.44.197.69.61.537.91zm2.446 2.729c-.287.267-.85.143-1.232-.28-.396-.42-.47-.983-.177-1.254.298-.266.844-.14 1.24.28.394.426.472.984.17 1.255zM31.312 98.012c-.37.258-.976.017-1.35-.52-.37-.538-.37-1.183.01-1.44.373-.258.97-.025 1.35.507.368.545.368 1.19-.01 1.452zm3.261 3.361c-.33.365-1.036.267-1.552-.23-.527-.487-.674-1.18-.343-1.544.336-.366 1.045-.264 1.564.23.527.486.686 1.18.333 1.543zm4.5 1.951c-.147.473-.825.688-1.51.486-.683-.207-1.13-.76-.99-1.238.14-.477.823-.7 1.512-.485.683.206 1.13.756.988 1.237zm4.943.361c.017.498-.563.91-1.28.92-.723.017-1.308-.387-1.315-.877 0-.503.568-.91 1.29-.924.717-.013 1.306.387 1.306.88zm4.598-.782c.086.485-.413.984-1.126 1.117-.7.13-1.35-.172-1.44-.653-.086-.498.422-.997 1.122-1.126.714-.123 1.354.17 1.444.663zm0 0" />
             </g>
           </svg>
-          <div className="absolute bottom-0 text-white underline opacity-75 hover:opacity-100">
-            Github
-          </div>
-        </a>
-        {live && (
           <a
-            href={live}
+            href={github}
             target="_blank"
+            className="absolute bottom-0 text-white underline opacity-75 hover:opacity-100"
+          >
+            Github
+          </a>
+        </div>
+        {live && (
+          <div
             className="transition h-64 w-[1000px] lg:h-80 bg-blue-500 flex justify-center items-center"
             style={{ opacity: active ? 0.9 : 0 }}
           >
@@ -71,10 +71,14 @@ function Project({ name, description, image, github, live }: ProjectProp) {
               <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path>
               <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>
             </svg>
-            <div className="absolute bottom-0 text-white underline opacity-75 hover:opacity-100">
+            <a
+              href={live}
+              target="_blank"
+              className="absolute bottom-0 text-white underline opacity-75 hover:opacity-100"
+            >
               Live Website
-            </div>
-          </a>
+            </a>
+          </div>
         )}
       </div>
       <img className="object-cover h-64 w-full lg:h-80" src={image}></img>
