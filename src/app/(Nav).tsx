b18d84888faa6523c023c406668f0433a55f5d6d
Yosua Nicolaus <yosuanicolaus@gmail.com>
2023-03-12 23:54:00 +1100

    add and update resume with the newest version

--- src/app/(Nav).tsx
+++ src/app/(Nav).tsx
@@ -2,8 +2,8 @@ export function Nav() {
   return (
     <nav className="flex p-8">
       <a
-        href={"/yosua-nicolaus-resume.pdf"}
-        download="yosua-nicolaus-resume.pdf"
+        href="Yosua_Nicolaus_Resume.pdf"
+        download="Yosua_Nicolaus_Resume.pdf"
         className="text-xl font-thin hover:text-green-600 transition"
       >
         Resume
